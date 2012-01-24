8665f2a37a982c25fd2f744d948e4c31a407b997
Mykhailo Moroz <morozmike@gmail.com>
2012-01-24 16:36:20 +0200

    Added EngToolsStatusUpdater as post-join action.

--- src/main/java/join/JoinTrigger.java
+++ src/main/java/join/JoinTrigger.java
@@ -328,7 +328,13 @@ public class JoinTrigger extends Recorder implements DependecyDeclarer, MatrixAg
             if (copyArchiver != null) {
                 list.add(Hudson.getInstance().getDescriptorByType(com.thalesgroup.hudson.plugins.copyarchiver.CopyArchiverPublisher.CopyArchiverDescriptor.class));
             }
-            // See issue 4384.  Supporting the mailer here breaks its use as the regular post-build action.
+            // EngTools Status Updater
+            Plugin engTools = Hudson.getInstance().getPlugin("engtools");
+            if (engTools != null) {
+                list.add(Hudson.getInstance().getDescriptorByType(com.cisco.hudson.plugins.EngToolsStatusUpdater.DescriptorImpl.class));
+            }
+
+	    // See issue 4384.  Supporting the mailer here breaks its use as the regular post-build action.
             //list.add(Hudson.getInstance().getDescriptorByType(hudson.tasks.Mailer.DescriptorImpl.class));
             return list;
         }
